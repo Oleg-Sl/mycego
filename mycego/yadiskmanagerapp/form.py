7a44a56d983fb528186e4b059d93899f05503087
Oleg <slepcov_oleg@mail.ru>
2024-09-27 22:31:23 +0700

    Added label field and style for field forms

--- mycego/yadiskmanagerapp/form.py
+++ mycego/yadiskmanagerapp/form.py
@@ -2,13 +2,28 @@ from django import forms
 
 
 class ClientDataForm(forms.Form):
-    client_id = forms.CharField(max_length=255)
-    client_secret = forms.CharField(max_length=255)
+    client_id = forms.CharField(label='Client ID', max_length=255)
+    client_secret = forms.CharField(label='Client Secret', max_length=255)
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
 
 class CodeForm(forms.Form):
-    code = forms.IntegerField()
+    code = forms.IntegerField(label='Код доступа')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
 
 class PublicUrlForm(forms.Form):
-    url = forms.CharField(max_length=255)
+    url = forms.CharField(label='Ключ опубликованного ресурса или публичная ссылка на ресурс', max_length=255)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
