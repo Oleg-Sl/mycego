39bbc01f349f558854956896fa360963af30ee40
Oleg <slepcov_oleg@mail.ru>
2024-09-27 21:04:26 +0700

    Add function and class for working with ya disk

---
+++ mycego/yadiskmanagerapp/services/token_utils.py
@@ -0,0 +1,21 @@
+from requests_oauthlib import OAuth2Session
+
+
+AUTH_URL = "https://oauth.yandex.ru/authorize"
+TOKEN_URL = "https://oauth.yandex.ru/token"
+
+
+def get_authorization_url(client_id):
+    oauth = OAuth2Session(client_id=client_id)
+    authorization_url, state = oauth.authorization_url(AUTH_URL, force_confirm="true")
+    return authorization_url
+
+
+def get_token(client_id, client_secret, code):
+    oauth = OAuth2Session(client_id=client_id)
+    token = oauth.fetch_token(
+        token_url=TOKEN_URL,
+        code=code,
+        client_secret=client_secret
+    )
+    return token
--- mycego/yadiskmanagerapp/services/ya_disk_api.py
+++ mycego/yadiskmanagerapp/services/ya_disk_api.py
@@ -1,27 +1,26 @@
+import requests
 from requests_oauthlib import OAuth2Session
-
-# https://oauth.yandex.ru/client/7a477dfee8fe4bf79e5349aff963389e
-# https://education.yandex.ru/handbook/python/article/modul-requests
-client_id = '7a477dfee8fe4bf79e5349aff963389e'
-client_secret = '94cf003c71c845788e2bb06c1f79c245'
-token = 'y0_AgAEA7qj8UkqAAyDBgAAAAESTEIXAABHfKNWH8RAP4OHIhc8yD6FsaRRYA'
 
 
 class YaDiskAPI:
+    API = 'https://cloud-api.yandex.net/v1/disk/'
+
     def __init__(self, token):
         self.token = token
 
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': self.token
+        }
+    
+    def get_public_resources(self, public_key=None):
+        url = self.API + 'public/resources'
+        headers = self.get_headers()
+        params = {
+            'public_key': public_key,
+        }
+        response = requests.get(url, headers=headers, params=params)
+        return response.json()
 
-if __name__ == '__main__':
-    # ya_api = YaDiskAPI(token)
-    auth_url = "https://oauth.yandex.ru/authorize"
-    token_url = "https://oauth.yandex.ru/token"
-    oauth = OAuth2Session(client_id=client_id)
-    # authorization_url, state = oauth.authorization_url(auth_url, force_confirm="true")
-    # print("Перейдите по ссылке, авторизуйтесь и скопируйте код:", authorization_url)
-    code = input("Вставьте одноразовый код: ")
-    token = oauth.fetch_token(token_url=token_url,
-                            code=code,
-                            client_secret=client_secret)
-    access_token = token["access_token"]
-    print(access_token)
+    
